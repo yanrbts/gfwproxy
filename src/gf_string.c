377a6d6c9993acab786485bb8d779dadc3985722
yanrbts <772166784@qq.com>
2025-01-01 22:23:58 +0800

    add code

--- src/gf_string.c
+++ src/gf_string.c
@@ -28,3 +28,181 @@
 #include <stdlib.h>
 #include <string.h>
 #include <gf_core.h>
+
+void string_init(struct string *str) {
+    str->len = 0;
+    str->data = NULL;
+}
+
+void string_deinit(struct string *str) {
+    ASSERT((str->len == 0 && str->data == NULL) ||
+           (str->len != 0 && str->data != NULL));
+    
+    if (str->data != NULL) {
+        gf_free(str->data);
+        string_init(str);
+    }
+}
+
+bool string_empty(const struct string *str) {
+    ASSERT((str->len == 0 && str->data == NULL) ||
+           (str->len != 0 && str->data != NULL));
+    return str->len == 0;
+}
+
+rstatus_t string_duplicate(struct string *dst, const struct string *src) {
+    ASSERT(dst->len == 0 && dst->data == NULL);
+    ASSERT(src->len != 0 && src->data != NULL);
+
+    dst->data = gf_strndup(src->data, src->len+1);
+    if (dst->data == NULL) {
+        return GF_ENOMEM;
+    }
+
+    dst->len = src->len;
+    dst->data[dst->len] = '\0';
+
+    return GF_OK;
+}
+
+rstatus_t string_copy(struct string *dst, const uint8_t *src, uint32_t srclen) {
+    ASSERT(dst->len == 0 && dst->data == NULL);
+    ASSERT(src != NULL && srclen != 0);
+
+    dst->data = gf_strndup(src, srclen+1);
+    if (dst->data == NULL) {
+        return GF_ENOMEM;
+    }
+
+    dst->len = srclen;
+    dst->data[dst->len] = '\0';
+
+    return GF_OK;
+}
+
+int string_compare(const struct string *s1, const struct string *s2) {
+    if (s1->len != s2->len) {
+        return s1->len > s2->len ? 1 : -1;
+    }
+
+    return gf_strncmp(s1->data, s2->data, s1->len);
+}
+
+static const char *const hex = "0123456789abcdef";
+
+static char *
+_safe_utoa(int _base, uint64_t val, char *buf)
+{
+    uint32_t base = (uint32_t)_base;
+    *buf-- = 0;
+    do {
+        *buf-- = hex[val % base];
+    } while ((val /= base) != 0);
+    return buf + 1;
+}
+
+static char *
+_safe_itoa(int base, int64_t val, char *buf)
+{
+    char *orig_buf = buf;
+    const int32_t is_neg = (val < 0);
+    *buf-- = 0;
+
+    if (is_neg) {
+        val = -val;
+    }
+    if (is_neg && base == 16) {
+        int ix;
+        val -= 1;
+        for (ix = 0; ix < 16; ++ix)
+            buf[-ix] = '0';
+    }
+
+    do {
+        *buf-- = hex[val % base];
+    } while ((val /= base) != 0);
+
+    if (is_neg && base == 10) {
+        *buf-- = '-';
+    }
+
+    if (is_neg && base == 16) {
+        int ix;
+        buf = orig_buf - 1;
+        for (ix = 0; ix < 16; ++ix, --buf) {
+            /* *INDENT-OFF* */
+            switch (*buf) {
+            case '0': *buf = 'f'; break;
+            case '1': *buf = 'e'; break;
+            case '2': *buf = 'd'; break;
+            case '3': *buf = 'c'; break;
+            case '4': *buf = 'b'; break;
+            case '5': *buf = 'a'; break;
+            case '6': *buf = '9'; break;
+            case '7': *buf = '8'; break;
+            case '8': *buf = '7'; break;
+            case '9': *buf = '6'; break;
+            case 'a': *buf = '5'; break;
+            case 'b': *buf = '4'; break;
+            case 'c': *buf = '3'; break;
+            case 'd': *buf = '2'; break;
+            case 'e': *buf = '1'; break;
+            case 'f': *buf = '0'; break;
+            }
+            /* *INDENT-ON* */
+        }
+    }
+    return buf + 1;
+}
+
+static const char *
+_safe_check_longlong(const char *fmt, int32_t *have_longlong)
+{
+    *have_longlong = false;
+    if (*fmt == 'l') {
+        fmt++;
+        if (*fmt != 'l') {
+            *have_longlong = (sizeof(long) == sizeof(int64_t));
+        } else {
+            fmt++;
+            *have_longlong = true;
+        }
+    }
+    return fmt;
+}
+
+int
+_safe_vsnprintf(char *to, size_t size, const char *format, va_list ap)
+{
+    char *start = to;
+    char *end = start + size - 1;
+
+    for ( ; *format; ++format) {
+        int32_t have_longlong = false;
+        if (*format != '%') {
+            if (to == end) { /* end of buffer */
+                break;
+            }
+            *to++ = *format;    /* copy ordinary char */
+            continue;
+        }
+        ++format;               /* skip '%' */
+
+        format = _safe_check_longlong(format, &have_longlong);
+
+        switch (*format) {
+        case 'd':
+        case 'i':
+        case 'u':
+        case 'x':
+        case 'p':
+            {
+
+            }
+        case 's':
+            {
+                
+            }
+        }
+    }
+}
